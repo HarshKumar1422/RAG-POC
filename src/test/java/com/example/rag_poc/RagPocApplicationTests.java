4aa158c5193961ded13ea8d84341889a3f598114
Harsh Kumar <harshtyagi140801@gmail.com>
2025-12-25 10:38:14 +0530

    base version

---
+++ src/test/java/com/example/rag_poc/RagPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rag_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
